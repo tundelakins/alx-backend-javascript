8795659d5498ce09f79ecba4235cbc8991eecf85
tundelakins <tundelakins4real@yahoo.com>
2023-01-03 15:16:01 +0000

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author - Akins Tunde Kola <https://github.com/tundelakins>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}

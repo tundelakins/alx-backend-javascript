36948f67da96e7c09cdaabc44146c1003521f2fb
tundelakins <tundelakins4real@yahoo.com>
2023-01-04 15:28:31 +0000

    Typescript

--- 0x04-TypeScript/a/js/main.ts
+++ 0x04-TypeScript/a/js/main.ts
@@ -1,68 +1,12 @@
-export interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-export interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-export class Director implements DirectorInterface {
-  workFromHome() {
-    return 'Working from home';
-  }
-
-  getCoffeeBreak() {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks() {
-    return 'Getting to director tasks';
-  }
-}
-
-export class Teacher implements TeacherInterface {
-  workFromHome() {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak() {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks() {
-    return 'Getting to work';
-  }
-}
-
-export function createEmployee(salary: (number | string)): (Director | Teacher) {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
-}
-
-export function isDirector(employee: (Director | Teacher)) {
-  return employee instanceof Director;
-}
-
-export function executeWork(employee: (Director | Teacher)) {
-  if (isDirector(employee)) {
-    return (employee as Director).workDirectorTasks();
-  }
-  return (employee as Teacher).workTeacherTasks();
-}
-
-export type Subjects = ('Math' | 'History');
-
-export function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  }
-  if (todayClass === 'History') {
-    return 'Teaching History';
-  }
-}
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {...row, age: 23};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
